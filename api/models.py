a1c059cd07d1f3ac1a458147de4779504589200e
WeNeedThePoh <pedromealha@hotmail.com>
2018-07-29 18:15:45 +0100

    add choices for category field

--- api/models.py
+++ api/models.py
@@ -3,11 +3,19 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Occurence(models.Model):
+    CATEGORIES = (
+        ('CONSTRUCTION', 'CONSTRUCTION'),
+        ('SPECIAL_EVENT', 'SPECIAL_EVENT'),
+        ('INCIDENT', 'INCIDENT'),
+        ('WEATHER_CONDITION', 'WEATHER_CONDITION'),
+        ('ROAD_CONDITION', 'ROAD_CONDITION'),
+    )
+
     author = models.CharField(max_length=255, null=False)
     description = models.CharField(max_length=255, null=False)
     createdData = models.DateTimeField(auto_now_add=True)
     updatedDate = models.DateTimeField(auto_now_add=True)
-    category = models.CharField(max_length=255, null=False)
+    category = models.CharField(max_length=255, choices=CATEGORIES, null=False)
     status = models.CharField(max_length=255, null=False)
 
     def __str__(self):
--- api/serializers.py
+++ api/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 from api.models import Occurence
 
-class OccurenceSerializer(serializers.HyperlinkedModelSerializer):
+class OccurenceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Occurence
         fields = ('id', 'author', 'description', 'createdData', 'updatedDate', 'category', 'status')
