909b3dd8b2e2514cb7c2ebe1fc18a7e0d348fb6e
WeNeedThePoh <pedromealha@hotmail.com>
2018-07-29 18:29:53 +0100

    add choices for status field

--- api/models.py
+++ api/models.py
@@ -11,12 +11,18 @@ class Occurence(models.Model):
         ('ROAD_CONDITION', 'ROAD_CONDITION'),
     )
 
+    STATUS = (
+        ('To be validated', 'To be validated'),
+        ('Validated', 'Validated'),
+        ('Solved', 'Solved'),
+    )
+
     author = models.CharField(max_length=255, null=False)
     description = models.CharField(max_length=255, null=False)
     createdData = models.DateTimeField(auto_now_add=True)
-    updatedDate = models.DateTimeField(auto_now_add=True)
+    updatedDate = models.DateTimeField(auto_now=True)
     category = models.CharField(max_length=255, choices=CATEGORIES, null=False)
-    status = models.CharField(max_length=255, null=False)
+    status = models.CharField(max_length=255, choices=STATUS, null=False, default='To be validated')
 
     def __str__(self):
         return "{} - {} - {} - {} - {} - {}".format(self.author, self.description, self.createdData, self.updatedDate, self.category, self.status)
